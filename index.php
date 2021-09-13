44bede6a81a058a48112459705bdf01b5d962d19
gokuschoyi <78709551+gokuschoyi@users.noreply.github.com>
2021-09-13 18:41:43 +1000

    Adding all files for testing on heroku

--- index.php
+++ index.php
@@ -47,7 +47,8 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Andada&amp;display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Andika&amp;display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre&amp;display=swap">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Courier+Prime:400,400i,700,700i&amp;subset=latin-ext&amp;display=swap">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Courier+Prime:400,400i,700,700i&amp;subset=latin-ext&amp;display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=GFS+Neohellenic&amp;display=swap">
     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
@@ -62,56 +63,78 @@
 
 <body style="height: 1020px;background: url(&quot;assets/img/6677.jpg&quot;) center no-repeat;">
     <nav class="navbar navbar-light navbar-expand-md navigation-clean" style="height: 150px;background: #f8ddf5;">
-        <div class="container"><a class="navbar-brand font-monospace" href="#" style="font-size: 35px;">The Wild Vet</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="container"><a class="navbar-brand font-monospace" href="#" style="font-size: 35px;">The Wild
+                Vet</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span
+                    class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navcol-1">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link font-monospace" href="clientCheckin.php">Check-In Page</a></li>
+                    <li class="nav-item"><a class="nav-link font-monospace" href="clientCheckin.php">Check-In Page</a>
+                    </li>
                     <li class="nav-item"><a class="nav-link font-monospace" href="#">About Us</a></li>
                     <li class="nav-item"><a class="nav-link font-monospace" href="contactUs.html">Contact</a></li>
                 </ul>
             </div>
         </div>
     </nav>
-    <h4 class="font-monospace text-center d-flex d-lg-flex justify-content-center align-items-center justify-content-lg-center align-items-lg-center" style="height: 133px;font-weight: bold;opacity: 0.85;font-size: 28.376px;">Online Check-In Management System</h4>
-    <div class="container" style = "height : 560px">
-        <div class="row d-md-flex d-lg-flex d-xl-flex justify-content-md-center align-items-md-center justify-content-lg-center align-items-lg-center justify-content-xl-center align-items-xl-center" style = "height : 260px">
+    <h4 class="font-monospace text-center d-flex d-lg-flex justify-content-center align-items-center justify-content-lg-center align-items-lg-center"
+        style="height: 133px;font-weight: bold;opacity: 0.85;font-size: 28.376px;">Online Check-In Management System
+    </h4>
+    <div class="container" style="height : 560px">
+        <div class="row d-md-flex d-lg-flex d-xl-flex justify-content-md-center align-items-md-center justify-content-lg-center align-items-lg-center justify-content-xl-center align-items-xl-center"
+            style="height : 260px">
             <div class="col-md-6 col-lg-4 item" style="height: 184px;">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-12 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center align-items-xl-center"><img src="assets/img/receptionist.png" style="height: 60px;"></div>
+                        <div
+                            class="col-md-12 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center align-items-xl-center">
+                            <img src="assets/img/receptionist.png" style="height: 60px;"></div>
                     </div>
                     <div class="row">
                         <div class="col-md-12" style="height: 70px;">
-                            <h2 class="font-monospace d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center align-items-xl-center" style="color: rgb(93,72,96);height: 60px;">Receptionist</h2>
+                            <h2 class="font-monospace d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center align-items-xl-center"
+                                style="color: rgb(93,72,96);height: 60px;">Receptionist</h2>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-12 d-flex d-lg-flex d-xl-flex justify-content-center align-items-center justify-content-lg-center align-items-lg-center justify-content-xl-center align-items-xl-center"><a class="btn btn-primary" role="button" style="border-radius: 30px;" href="receptionistLogin.php">Login</a></div>
+                        <div
+                            class="col-md-12 d-flex d-lg-flex d-xl-flex justify-content-center align-items-center justify-content-lg-center align-items-lg-center justify-content-xl-center align-items-xl-center">
+                            <a class="btn btn-primary" role="button" style="border-radius: 30px;"
+                                href="receptionistLogin.php">Login</a></div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row d-md-flex d-lg-flex d-xl-flex justify-content-md-center align-items-md-center justify-content-lg-center align-items-lg-center justify-content-xl-center align-items-xl-center" style = "height : 260px">
+        <div class="row d-md-flex d-lg-flex d-xl-flex justify-content-md-center align-items-md-center justify-content-lg-center align-items-lg-center justify-content-xl-center align-items-xl-center"
+            style="height : 260px">
             <div class="col-md-6 col-lg-4 item" style="height: 184px;">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-12 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center align-items-xl-center"><img src="assets/img/doctor.png" style="height: 60px;"></div>
+                        <div
+                            class="col-md-12 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center align-items-xl-center">
+                            <img src="assets/img/doctor.png" style="height: 60px;"></div>
                     </div>
                     <div class="row">
                         <div class="col-md-12" style="height: 70px;">
-                            <h2 class="font-monospace d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center align-items-xl-center" style="color: rgb(93,72,96);height: 60px;">Doctor</h2>
+                            <h2 class="font-monospace d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center align-items-xl-center"
+                                style="color: rgb(93,72,96);height: 60px;">Doctor</h2>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-12 d-flex d-lg-flex d-xl-flex justify-content-center align-items-center justify-content-lg-center align-items-lg-center justify-content-xl-center align-items-xl-center"><a class="btn btn-primary" role="button" style="border-radius: 30px;" href="doctorLogin.php">Login</a></div>
+                        <div
+                            class="col-md-12 d-flex d-lg-flex d-xl-flex justify-content-center align-items-center justify-content-lg-center align-items-lg-center justify-content-xl-center align-items-xl-center">
+                            <a class="btn btn-primary" role="button" style="border-radius: 30px;"
+                                href="doctorLogin.php">Login</a></div>
                     </div>
                 </div>
             </div>
         </div>
-        
+
     </div>
     <footer class="footer-basic" style="height: 200px;">
-        <div class="social" style="height: 60px;"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+        <div class="social" style="height: 60px;"><a href="#"><i class="icon ion-social-instagram"></i></a><a
+                href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i
+                    class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a>
+        </div>
         <ul class="list-inline">
             <li class="list-inline-item"><a href="#">Home</a></li>
             <li class="list-inline-item"><a href="#">Services</a></li>
