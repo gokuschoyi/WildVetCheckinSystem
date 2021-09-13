eba11b23b50c1ed3d0dd06aba8b54f1f8fcf2aca
gokuschoyi <78709551+gokuschoyi@users.noreply.github.com>
2021-09-13 20:47:53 +1000

    bs test

--- includes/header.php
+++ includes/header.php
@@ -17,7 +17,7 @@
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.11.0/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" type= "text/css" href="\assets\css\sb-admin-2.css">
+    <link rel="stylesheet" type= "text/css" href="\assets\bootstrap\css\sb-admin-2.css">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"> </script>
     <style>
