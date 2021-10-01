c53a341c9e47f9af826a966e22aeb2ce6a45f97b
gokuschoyi <78709551+gokuschoyi@users.noreply.github.com>
2021-10-01 13:02:35 +1000

    Correction on CROS and Testing Heroku deploy

--- dataA.php
+++ dataA.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 $conn = new mysqli('pk1l4ihepirw9fob.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'sn4abkagkvz8sd1n','nm85ad3jt3wpvxc6','xlx8er1i5yj6m7u4');
 $sqlQuery = "SELECT EXTRACT(day FROM checkinDate) AS day , COUNT(clientID) AS totalC FROM clientinfo GROUP BY EXTRACT(day FROM checkinDate);";
 
--- dataB.php
+++ dataB.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 $conn = new mysqli('pk1l4ihepirw9fob.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'sn4abkagkvz8sd1n','nm85ad3jt3wpvxc6','xlx8er1i5yj6m7u4');
 $sqlQuery = "SELECT checkinDate as day, count(clientID) as totalC from clientinfo WHERE checkinDate BETWEEN CURDATE()-14 AND CURDATE() GROUP BY EXTRACT(day FROM checkinDate)";
 
